7b326139fcfb352e4056d6a8d2a937a398ad9cf6
David Riley <Dril112191@gmail.com>
2022-11-12 18:34:59 -0500

    Added conversion
    Added ounces to cups and inches to centimeters

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let convert = document.getElementById("convert")
 let length = document.getElementById("length2")
 let volume = document.getElementById("volume2")
 let mass = document.getElementById("mass2")
+let inches = document.getElementById("inches")
+let ounces = document.getElementById("cups")
 let inputNum = 0
 
 
@@ -11,7 +13,8 @@ convert.addEventListener("click", function(){
     length.innerHTML = `${inputNum} meters = ${convertLength()} feet | ${inputNum} feet = ${feetToMeters()} meters`
     volume.innerHTML = `${inputNum} liters = ${convertVolume()} gallons | ${inputNum} gallons = ${gallonsToLiters()} liters`
     mass.innerHTML = `${inputNum} kilos = ${convertMass()} pounds | ${inputNum} pounds = ${poundsToKilos()} kilos`
-    
+    inches.innerHTML = `${inputNum} centimeters = ${centermetersToInches()} inches | ${inputNum} inches = ${inchesToCentimeters()} centimeters`
+    ounces.innerHTML = `${inputNum} ounces = ${ouncesToCups()} cups | ${inputNum} cups = ${cupsToOunces()} ounces`
 })
 
 function convertLength(){
@@ -46,6 +49,30 @@ function gallonsToLiters(){
 
 function poundsToKilos(){
     let tempNum = 0.453592
+    let calc = inputNum * tempNum
+    return calc.toFixed(3)
+}
+
+function inchesToCentimeters(){
+    let tempNum = 2.54
+    let calc = inputNum * tempNum
+    return calc.toFixed(3)
+}
+
+function centermetersToInches() {
+    let tempNum = 0.393701
+    let calc = inputNum * tempNum
+    return calc.toFixed(3)
+}
+
+function ouncesToCups() {
+    let tempNum = 0.125
+    let calc = inputNum * tempNum
+    return calc.toFixed(3)
+}
+
+function cupsToOunces() {
+    let tempNum = 8
     let calc = inputNum * tempNum
     return calc.toFixed(3)
 }
